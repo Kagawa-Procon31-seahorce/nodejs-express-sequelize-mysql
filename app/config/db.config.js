32a59ba696186acf91a437d4f73718663c5bef5b
Akira_Onishi <ao24@gmx.com>
2020-09-23 01:54:25 +0900

    db情報変更3

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "lus-cdbr-east-02.cleardb.com",
+  HOST: "us-cdbr-east-02.cleardb.com",
   USER: "b53e600c2df1f9",
   PASSWORD: "148db343",
   DB: "heroku_5d5bba4dd1d3b1a",
