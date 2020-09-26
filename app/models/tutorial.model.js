fe1b00630f187c30710985ce0954eea93c9b31e1
Akira_Onishi <ao24@gmx.com>
2020-09-26 21:59:46 +0900

    DB項目変更に対応

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -1,13 +1,49 @@
 module.exports = (sequelize, Sequelize) => {
   const Tutorial = sequelize.define("tutorial", {
-    title: {
+    user_id: {
       type: Sequelize.STRING
     },
-    description: {
-      type: Sequelize.STRING
+    to_port_code: {
+      type: Sequelize.INTEGER
     },
-    published: {
+    from_port_code: {
+      type: Sequelize.INTEGER
+    },
+    allow_share_ride: {
       type: Sequelize.BOOLEAN
+    },
+    reservations_number: {
+      type: Sequelize.INTEGER
+    },
+    departure_date: {
+      type: Sequelize.DATE
+    },
+    departure_time: {
+      type: Sequelize.TIME
+    },
+    arrival_time: {
+      type: Sequelize.TIME
+    },
+    reservation_status: {
+      type: Sequelize.INTEGER
+    },
+    price: {
+      type: Sequelize.INTEGER
+    },
+    allocation_code: {
+      type: Sequelize.INTEGER
+    },
+    cancel_status: {
+      type: Sequelize.BOOLEAN
+    },
+    captain_code: {
+      type: Sequelize.INTEGER
+    },
+    boat_code: {
+      type: Sequelize.INTEGER
+    },
+    taxi_company_code: {
+      type: Sequelize.INTEGER
     }
   });
 
