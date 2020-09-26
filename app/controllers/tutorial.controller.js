e4090e242fb1c3586f50d849c11534fcbfdc2c21
r_take <r.takegami.0330@gmail.com>
2020-09-27 00:59:06 +0900

    エラー検証

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -78,21 +78,6 @@ exports.findOne = (req, res) => {
     });
 };
 
-// Find reservations with a from id
-exports.findOne = (req, res) => {
-  const id = req.params.id;
-
-  Tutorial.findByPk(id)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving Tutorial with id=" + id
-      });
-    });
-};
-
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {
   const id = req.params.id;
