13e263f3ec7751bec66c7843a1a1b0bc8fd6c7a1
r_take <r.takegami.0330@gmail.com>
2020-09-27 18:06:49 +0900

    実験

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -48,10 +48,10 @@ exports.create = (req, res) => {
 
 // Retrieve all Tutorials from the database.
 exports.findAllByUser = (req, res) => {
-  const user_id = req.query.user_id;
+  const user_id = req.query.user;
   var condition = user_id ? { user_id: { [Op.like]: `%${user_id}%` } } : null;
 
-  Tutorial.findAll({ where: condition })
+  Tutorial.findAllByUser({ where: condition })
     .then(data => {
       res.send(data);
     })
@@ -65,10 +65,10 @@ exports.findAllByUser = (req, res) => {
 
 // Retrieve all Tutorials from the database.
 exports.findAllByPort = (req, res) => {
-  const from_port_code = req.query.user_id;
+  const from_port_code = req.query.from;
   var condition = from_port_code ? { from_port_code: { [Op.like]: `%${from_port_code}%` } } : null;
 
-  Tutorial.findAll({ where: condition })
+  Tutorial.findAllByPort({ where: condition })
     .then(data => {
       res.send(data);
     })
