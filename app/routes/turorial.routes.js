274a5fb65d4d5e2129e7d8d81cf95bd2da3dd04b
r_take <r.takegami.0330@gmail.com>
2020-09-28 12:54:53 +0900

    エラー修正

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -7,7 +7,7 @@ module.exports = app => {
   router.post("/", tutorials.create);
 
   // Retrieve all Tutorials
-  router.get("/", tutorials.findAll);
+  router.get("/", tutorials.findAllByUser);
   router.get("/", tutorials.findAllByPort);
   router.get("/", tutorials.findAllByTime);
 
